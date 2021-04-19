6b1716322bde2c9f6d52fce6eac5ffd022d58cda
Jan Schering <jan.schering@blueonic.com>
2021-04-19 21:22:28 +0200

    implementing the replayOnExperience functionality for the new AI model

--- AI_v2/model_functions.ts
+++ AI_v2/model_functions.ts
@@ -1,6 +1,7 @@
 import * as tf from "@tensorflow/tfjs";
+import { Tensor } from "@tensorflow/tfjs";
 import { Player } from "../game/player";
-import { NUM_ACTIONS } from "./model_constants";
+import { NUM_ACTIONS, NUM_INPUTS, NUM_OUTPUTS } from "./model_constants";
 import { Memory } from "./model_memory";
 
 /**
@@ -40,7 +41,7 @@ const getRandomInt = (max: number): number => {
  * @description Allows the model to replay the game saved in the Memory Object and learn from it.
  * @param props - The metadata about the last game.
  */
-const replayExperience = (props: TrainingProps) => {
+const replayExperience = async (props: TrainingProps) => {
   //We want to implement deep Q-learning in here.
 
   const { model_memory, model, discountFactor } = props;
@@ -48,6 +49,64 @@ const replayExperience = (props: TrainingProps) => {
   // First we need a batch of examples from the last game.
   // For now we are going with a standard batch size of 32.
   const batch: Memory = sampleFromMemory(model_memory, 32);
+
+  // We create arrays to gather the input-states and calculated labels/outputs,
+  // So that we can fit the model parameters to the collected data.
+  let x: Array<Array<number>> = [];
+  let y: Array<Array<number>> = [];
+
+  /*
+   * For each Sample in the batch, we want to update the Q-Values:
+   * The Q value can be calculated by comparing the predctions of the
+   * model for a given state with the actual reward received.
+   *
+   * To take future rewards into account, we also look at the next state and the predicted
+   * rewards. This gets weighted by the discount
+   * factor, as these predictions hold uncertainty and shouldnt't hold the same weight as the
+   * immediate received reward.
+   */
+  batch.forEach((sample) => {
+    const { state, nextState, action, reward } = sample;
+    // We let the model predict the rewards of the current state.
+    const current_Q: any = model.predict(state);
+
+    // We also let the model predict the rewards for the next state, if there was a next state in the game.
+    let future_reward = tf.zeros([NUM_ACTIONS]);
+    if (nextState) {
+      future_reward = <Tensor>model.predict(nextState);
+    }
+
+    /*
+     * We now make use of the fact, that for this particular state, we were able to observe the actually
+     * observe what happens, when the model chooses one of the options. We saved what option the model chose in the game
+     * and what happened afterwards. We can update the Q-Value for that particular combination of state and action.
+     * To approximate the future reward that this course of action generates, we add the highest prediction of the next state.
+     */
+    current_Q[action] =
+      reward + discountFactor * future_reward.max().dataSync()[0];
+
+    // We can now push the state to the input collector
+    x.push(<number[]>(<unknown>state.dataSync()));
+    // For the labels/outputs, we push the updated Q values
+    y.push(current_Q.dataSync());
+
+    // at the end we have to take care of disposing of the created Tensors
+    current_Q.dispose();
+    future_reward.dispose();
+  });
+
+  /*
+   * Having updated the Q-Values and gathered all input/output pairs of the batch,
+   * We can reshape the collectors and prepare them for training the model.
+   */
+  const inputs = tf.tensor2d(x, [x.length, NUM_INPUTS]);
+  const labels = tf.tensor2d(y, [y.length, NUM_OUTPUTS]);
+
+  // Use the inputs and labels to train the model
+  await model.trainOnBatch(inputs, labels);
+
+  inputs.dispose();
+  labels.dispose();
 };
 
 /**
